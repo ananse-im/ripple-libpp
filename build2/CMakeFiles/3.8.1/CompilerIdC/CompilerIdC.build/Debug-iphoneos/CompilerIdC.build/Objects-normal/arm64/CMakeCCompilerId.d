fb9ac235d9efa522fd7e0afd50b4b0e10ee2da1a
Nguyen Tuan <anhtuanpk2000@gmail.com>
2017-10-09 12:07:44 +0700

    stop tracking generated files

--- build2/CMakeFiles/3.8.1/CompilerIdC/CompilerIdC.build/Debug-iphoneos/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/TuanNguyen/Working/ShizzleStudio/Ripple/build2/CMakeFiles/3.8.1/CompilerIdC/CMakeCCompilerId.c
--- build2/ripple-libpp.build/Debug-iphonesimulator/ZERO_CHECK.build/Script-6FA936E2705145579811F848.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/TuanNguyen/Working/ShizzleStudio/Ripple/build2 -f /Users/TuanNguyen/Working/ShizzleStudio/Ripple/build2/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
